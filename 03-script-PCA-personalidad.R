4d9b1ab3c4b5fd58a84e0023f60ea11a794bd580
Luis Auza <luis.auza@gmail.com>
2023-03-13 23:41:53 +0000

    ggcorrplot

--- 03-script-PCA-personalidad.R
+++ 03-script-PCA-personalidad.R
@@ -1,6 +1,6 @@
+library(ggcorrplot)
 
 # En este tema vamos a hacer uso del DF6 como base
-
 # Paso previo: Correlaciones ----
 
 ## Convirtiendo frases2 en vector
@@ -9,4 +9,21 @@ frases3 <- as.vector(frases2)
 ## Matriz de correlaciones ----
 r <- cor(x = DF6 %>% select(all_of(frases3)),
          method = "spearman")
-View(r)
+
+
+## Gráfico de matriz de correlaciones
+
+ggplotly(
+  
+  ggcorrplot(corr = r,
+             type = "upper",
+             show.legend = F,
+             colors = c("red", "white", "blue"),
+             tl.cex = 14) +
+    
+    theme(axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          panel.grid.major.x = element_blank())
+  
+)
+
